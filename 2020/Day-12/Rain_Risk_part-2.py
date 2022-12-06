6df61a6c9dc7cb08f838d96de57fca1fe6d2bf29
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2022-12-06 15:00:22 -0600

    Add solution to part 2

--- 2020/Day-12/Rain_Risk_part-2.py
+++ 2020/Day-12/Rain_Risk_part-2.py
@@ -1,40 +1,45 @@
-def execute(direction, status):
+def execute(direction, ship, nav):
     d = direction[0]
     q = int(direction[1:])
     match d:
         case 'N':
-            status[1] += q
+            nav[1] += q
         case 'E':
-            status[0] += q
+            nav[0] += q
         case 'S':
-            status[1] -= q
+            nav[1] -= q
         case 'W':
-            status[0] -= q
+            nav[0] -= q
         case 'F':
-            match status[2]:
-                case 0:
-                    status[1] += q
-                case 90:
-                    status[0] += q
-                case 180:
-                    status[1] -= q
-                case 270:
-                    status[0] -= q
+            ship[0] += q*nav[0]
+            ship[1] += q*nav[1]
         case 'L':
-            status[2] -= q
-            if status[2] < 0:
-                status[2] += 360
+            rotate(nav, q//90, False)
         case 'R':
-            status[2] += q
-            if status[2] >= 360:
-                status[2] -= 360
+            rotate(nav, q//90, True)
+
+def rotate(nav, n, clockwise):
+    for i in range(n):
+        t_x = nav[0]
+        t_y = nav[1]
+        if clockwise:
+            nav[0] = t_y 
+            nav[1] = -t_x
+        else:
+            nav[0] = -t_y
+            nav[1] = t_x
+        
+    
 
 with open('input.txt') as file:
     data = file.read().split("\n")
 
-status = [0,0,90] #[X,Y,THETA]
+ship_status = [0,0] #[X,Y]
+nav_status = [10,1] #[X,Y]
+
 
 for direction in data:
-    execute(direction, status)
+    execute(direction, ship_status, nav_status)
 
-print(f"Manhattan Distance: {abs(status[0]) + abs(status[1])}")
+print(f"Manhattan Distance: {abs(ship_status[0]) + abs(ship_status[1])}")
+
