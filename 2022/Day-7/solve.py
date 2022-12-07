99bcca4d0d379d5781b8192665d34a303cfbfb0b
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2022-12-06 20:25:39 -0600

    Add folder, solve, and input files for Day 7

---
+++ 2022/Day-7/solve.py
@@ -0,0 +1,16 @@
+def part1(data):
+    data = data.split("\n")
+    return
+
+
+
+def part2(data):
+    data = data.split("\n")
+    return
+
+with open("input.txt") as file:
+    data = file.read()
+
+
+res1, res2 = part1(data), part2(data)
+print(f"Part 1: {res1}\nPart 2: {res2}")
