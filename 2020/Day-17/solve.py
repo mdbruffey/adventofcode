f1743ef67400c43ec3a328071354b063311c7120
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2022-12-08 16:02:09 -0600

    Create directory, solve.py, and input.txt for 2020 Day 17

---
+++ 2020/Day-17/solve.py
@@ -0,0 +1,12 @@
+def part1(data):
+    pass
+
+def part2(data):
+    pass
+
+with open('input.txt') as file:
+    data = file.read()
+
+res1 = part1(data)
+res2 = part2(data)
+print(f"Part 1: {res1}\nPart 2: {res2}")
