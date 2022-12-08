2065741042cd40a9b0476ec3aae228342ccad920
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2022-12-07 20:05:43 -0600

    Add solution to Day 16 part 1

--- 2020/Day-16/solve.py
+++ 2020/Day-16/solve.py
@@ -1,5 +1,39 @@
+def has_valid_fields(val, rdict):
+	for key in rdict.keys():
+		if in_range(val, rdict[key]):
+			return True
+			
+	return False
+	
+def in_range(val, ranges):
+	a1, b1 = map(int, ranges[0].split("-"))
+	a2, b2 = map(int, ranges[1].split("-"))
+	return (val > a1 and val < b1) or (val > a2 and val < b2)
+
+def validate(ticket, rdict):
+	ticket = list(map(int,ticket.split(",")))
+	errors = 0
+	for val in ticket:
+		if not has_valid_fields(val, rdict):
+			errors += val
+			
+	return errors
+
 def part1(data):
-    pass
+    rules, my_ticket, tickets = data.split("\n\n")
+    rules = rules.split("\n")
+    rdict = {}
+    for line in rules:
+    	field, ranges = line.split(": ")
+    	ranges = ranges.split(" or ")
+    	rdict[field] = ranges
+    reference = my_ticket.split("\n")[1].split(",")
+    tickets = tickets.split("\n")[1:]
+    errors = 0
+    for ticket in tickets:
+    	errors += validate(ticket, rdict)
+    
+    return errors
 
 def part2(data):
     pass
