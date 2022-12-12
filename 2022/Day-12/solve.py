62b64c9d05e30a49da65aa2a8c0b0d45d0d18507
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2022-12-11 23:20:38 -0600

    Add solve.py for 2022 Day 12

---
+++ 2022/Day-12/solve.py
@@ -0,0 +1,13 @@
+def part1(data):
+    hmap = data.split("\n")
+    
+
+def part2(data):
+    pass
+
+with open("input.txt") as file:
+    data = file.read()
+
+res1 = part1(data)
+res2 = part2(data)
+print(f"Part 1: {res1}\nPart 2: {res2}")
