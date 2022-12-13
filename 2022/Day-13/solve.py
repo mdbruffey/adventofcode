980c86359d3347eec0156a85b18ca107f45cb8dc
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2022-12-13 00:51:56 -0600

    Add solution for 2022 Day 13 part 1
    However, function does not properly identify quite a few
    misordered packets... Has to do with how to handle equality cases
    I think I need a return None or something...

--- 2022/Day-13/solve.py
+++ 2022/Day-13/solve.py
@@ -1,7 +1,22 @@
-import ast
-
 def is_ordered(pair):
-    
+    left, right = pair
+    for i in range(0, min([len(left),len(right)])):
+        if left[i] == right[i]:
+            continue
+        elif type(left[i]) == int and type(right[i]) == int:
+            if left[i] > right[i]:
+                return False
+            elif left[i] < right[i]:
+                return True
+        elif type(left[i]) == list and type(right[i]) != list:
+            return is_ordered((left[i],[right[i]]))
+        elif type(left[i]) != list and type(right[i]) == list:
+            return is_ordered(([left[i]],right[i]))
+        else:
+            return is_ordered((left[i],right[i]))
+            
+    if len(left) > len(right):
+        return False
         
     return True
 
@@ -10,8 +25,8 @@ def part1(data):
     pairs = []
     for i, pair in enumerate(data):
         left, right = pair.split("\n")
-        left = ast.literal_eval(left)
-        right = ast.literal_eval(right)
+        left = eval(left)
+        right = eval(right)
         pairs.append((left,right))
         
     ordered = []
@@ -21,11 +36,32 @@ def part1(data):
     for i in range(len(ordered)):
         if ordered[i]:
             count += i+1
-    print(ordered)
     return count
 
 def part2(data):
-    pass
+    data = data.split("\n\n")
+    packets = []
+    for pair in data:
+        left, right = pair.split("\n")
+        left = eval(left)
+        right = eval(right)
+        packets.append(left)
+        packets.append(right)
+        
+    packets.append([[2]])
+    packets.append([[6]])
+    swapped = True
+    count = 0
+    while count < 500:
+        swapped = False
+        for i in range(len(packets)-1):
+            if not is_ordered( (packets[i],packets[i+1]) ):
+                packets[i], packets[i+1] = packets[i+1], packets[i]
+                swapped = True
+        count += 1
+    for packet in packets:
+        print(packet)
+    return (packets.index([[2]])+1)*(packets.index([[6]])+1)
 
 with open("input.txt") as file:
     data = file.read()
