2e17f87f6b141dad630403d1249ff01bc63cee2e
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2022-12-15 22:45:43 -0600

    Complete solution for 2022 Day 15 part 2
    Calculate the intercepts of the edges of each sensor's field,
    then look for pairs of intercepts that are 2 apart. Can find the
    blank spot easily this way (assumes that the only parallel field edges
    with intercepts within 2 of each other are not overlapping).

--- 2022/Day-15/solve.py
+++ 2022/Day-15/solve.py
@@ -25,11 +25,13 @@ def part1(data):
             detected.remove(x)
     return len(detected)
 
+def get_intercepts(x,y, m_range):
+    intercepts = []
 def part2(data):
     data = data.split('\n')
     sensor_ranges = {}
     beacons = list()
-    targ_y = 2000000
+    intercepts = []
     
     for line in data:
         sensor, beacon = line.split(": closest beacon is at ")
@@ -37,22 +39,21 @@ def part2(data):
         x1, y1 = map(int,sensor.split(","))
         beacon = beacon.replace("x=","").replace(" y=","")
         x2, y2 = map(int,beacon.split(","))
-        sensor_ranges[(x1,y1)] = abs(x2-x1) + abs(y2-y1)
-        beacons.append((x2,y2))
-    
-    for i in range(4000001):
-        if i%10000:
-            print(f"{i}/4,000,001")
-        detected = set()
-        for sensor in sensor_ranges:
-            x,y = sensor
-            width = 2*(sensor_ranges[sensor] - abs(y-i))+1
-            if width > 0:
-                detected = detected.union(set(range(x-width//2, x+width//2 +1)))
-        detected.difference_update(list(range(min(detected),max(detected)+1)))
-        if detected:
-            return detected[0], i
-	    
+        m_range = abs(x2-x1) + abs(y2-y1)
+        sensor_ranges[(x1,y1)] = m_range
+        intercepts.extend([(y1-m_range+x1, -1),
+                           (y1+m_range+x1,-1),
+                           (y1-m_range-x1,1),
+                           (y1+m_range-x1,1)])
+    intercepts.sort()
+    boundaries = []
+    for i in range(len(intercepts)-1):
+        if abs(intercepts[i][0]-intercepts[i+1][0]) == 2 and intercepts[i][1] == intercepts[i+1][1]:
+            boundaries.extend([intercepts[i][0],intercepts[i+1][0]])
+    x = (boundaries[2]-boundaries[0])//2
+    y = x + boundaries[0] + 1
+
+    return x*4000000 + y	    
 
 with open("input.txt") as file:
     data = file.read()
