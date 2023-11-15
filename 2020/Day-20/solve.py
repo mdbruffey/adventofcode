729aa78fdf24685732501d7ad50cbeef8ef1da65
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-11-15 17:32:15 -0600

    Some changes that still don't work
    I'm kinda stuck on this one right now, tbh.

--- 2020/Day-20/solve.py
+++ 2020/Day-20/solve.py
@@ -1,4 +1,5 @@
 import numpy
+import time
 
 class Tile:
     def __init__(self, t_id, t_image):
@@ -30,38 +31,42 @@ class Tile:
             for dir, side in tile.sides.items():
                 for d, s in self.sides.items():
                     if side == s or side[::-1] == s:
-                        self.neighbors[dir] = tile
+                        self.neighbors[d] = tile
                         matches += 1
                         break
         if matches >= 3:
             return
-        print(f"Tile {self.id}: {matches} matches")
         self.is_corner = True
         return
     
     def transpose(self, direction="left", flip=False):
         new_neighbors = {}
         if direction == "left":
+            #print("Rotating Left")
             new_neighbors["top"] = self.neighbors["right"]
             new_neighbors["right"] = self.neighbors["bottom"]
             new_neighbors["bottom"] = self.neighbors["left"]
             new_neighbors["left"] = self.neighbors["top"]
             if flip:
+                #print("and flipping...")
                 self.image = [[row[i] for row in self.image[::-1]] for i in range(len(self.image[0])-1,-1, -1)]
                 new_neighbors["left"], new_neighbors["right"] = new_neighbors["right"], new_neighbors["left"]
             else:
                 self.image = [[row[i] for row in self.image] for i in range(len(self.image[0])-1, -1, -1)]
         elif direction =="right":
+            #print("Rotating Right")
             new_neighbors["top"] = self.neighbors["left"]
             new_neighbors["right"] = self.neighbors["top"]
             new_neighbors["bottom"] = self.neighbors["right"]
             new_neighbors["left"] = self.neighbors["bottom"]
             if flip:
+                #print("and flipping...")
                 self.image = [[row[i] for row in self.image] for i in range(len(self.image[0]))]
                 new_neighbors["left"], new_neighbors["right"] = new_neighbors["right"], new_neighbors["left"]
             else:
                 self.image = [[row[i] for row in self.image[::-1]] for i in range(len(self.image[0]))]
         elif direction == "none" and flip:
+            #print("Just Flipping")
             new_neighbors["top"] = self.neighbors["top"]
             new_neighbors["right"] = self.neighbors["left"]
             new_neighbors["bottom"] = self.neighbors["bottom"]
@@ -69,28 +74,24 @@ class Tile:
             self.image = [[row[i] for i in range(len(self.image[0])-1,-1,-1)] for row in self.image]
 
         self.neighbors = new_neighbors
+        self.get_sides()
         return
     
-    def __str__(self):
-        string = f"Tile: {self.id}"
-        for row in self.image:
-            string += "\n" + "".join(row)
-        return string
-
-def assemble_row(start):
-    row = [start]
-    curr = start
-    next = start.neighbors["right"]
-    while next != None:
+    def find_next(self):
+        next = self.neighbors["right"]
+        if next == None:
+            return None
+        
         for dir, side in next.sides.items():
-            if curr.sides["right"] == side:
+            if self.sides["right"] == side:
                 if dir == "top":
                     next.transpose("right", True)
                 elif dir == "right":
                     next.transpose("none", True)
                 elif dir == "bottom":
                     next.transpose("right")
-            elif curr.sides["right"] == side[::-1]:
+                return next
+            elif self.sides["right"] == side[::-1]:
                 if dir == "top":
                     next.transpose("left")
                 elif dir == "right":
@@ -100,21 +101,85 @@ def assemble_row(start):
                     next.transpose("left", True)
                 elif dir == "left":
                     next.transpose("right", True)
-                    next.tranpose("left")
+                    next.transpose("left")
+                return next
+ 
+        print("Something has gone wrong. There are no matching sides?")
+        print(self)
+        print(next)
+        next.print_neighbors()
 
-        row.append(next)
-        curr = next
-        next = curr.neighbors["right"]
+    def find_bottom(self):
+        bottom = self.neighbors["bottom"]
+        if bottom == None:
+            return None
+        while bottom.neighbors["left"] != None:
+            bottom.transpose("left")
+        if bottom.neighbors["top"]:
+            t_id = bottom.neighbors["top"] .id
+        else:
+            t_id = None
+        if t_id != self.id:
+            bottom.transpose("right", True)
+            bottom.transpose("left")
+        return bottom
+    
+    def print_neighbors(self):
+        for dir, tile in self.neighbors.items():
+            if tile:
+                id = tile.id
+            else:
+                id = None
+            print(f"{dir}: {id}")
+
+    def __str__(self):
+        string = f"Tile: {self.id}"
+        for row in self.image:
+            string += "\n" + "".join(row)
+        return string
+
+
+def assemble_row(start):
+    row = []
+    curr = start
+    while curr:
+        row.append(curr)
+        curr = curr.find_next()
     return row
 
-def part1(data):
-    raw = data.split("\n\n")
-    tiles = []
-    for tile in raw:
-        t_id, info = tile.split(":")
-        t_id = int(t_id.split()[1])
-        t_image = info.split("\n")[1:]
-        tiles.append(Tile(t_id,t_image))
+def assemble_image(image):
+    comp = []
+    for row in image:
+        for i in range(1,len(row[0].image[0])-1):
+            line = []
+            for tile in row:
+                line.extend(tile.image[i][1:-1])
+            comp.append(line)
+
+    return comp
+
+
+                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   
+
+def count_dragons(image):
+    count = 0
+    for i in range(1,len(image)-1):
+        for j in range(0, len(image[0])-20):
+            l1 = image[i-1][j+19]
+            l2 = image[i][j] + image[i][j+5] + image[i][j+6] + image[i][j+11] + image[i][j+12] + image[i][j+17] + image[i][j+18] + image[i][j+19] + image[i][j+20]
+            l3 = image[i+1][j+1] + image[i+1][j+4] + image[i+1][j+7] + image[i+1][j+10] + image[i+1][j+13] + image[i+1][j+16]
+            if "." not in l1 and "." not in l2 and "." not in l3:
+                count += 1
+    return count
+
+def rotate(image):
+    image = [[row[i] for row in image] for i in range(len(image[0])-1, -1, -1)]
+    print(f"Image dimensions: {len(image[0])}x{len(image)}")
+    return image
+
+def part1(tiles):
     value = 1
     for tile in tiles:
         tile.find_matches(tiles)
@@ -123,14 +188,7 @@ def part1(data):
 
     return value
 
-def part2(data):
-    raw = data.split("\n\n")
-    tiles = []
-    for tile in raw:
-        t_id, info = tile.split(":")
-        t_id = int(t_id.split()[1])
-        t_image = info.split("\n")[1:]
-        tiles.append(Tile(t_id,t_image))
+def part2(tiles):
     corners = []
     for tile in tiles:
         tile.find_matches(tiles)
@@ -139,12 +197,40 @@ def part2(data):
     for corner in corners:
         if corner.neighbors["top"] == None and corner.neighbors["left"] == None:
             start = corner
-    row = assemble_row(start)
-    print(len(row))
+    image = []
+    while start != None:
+        row = assemble_row(start)
+        image.append(row)
+        start = start.find_bottom()
+
+    print(f"{len(image)*len(image[0])} tiles in the image")
+    image = assemble_image(image)
+    dragons = count_dragons(image)
+    if dragons == 0:
+        for i in range(3):
+            image = rotate(image)
+            dragons += count_dragons(image)
+    image = [[row[i] for i in range(len(image[0])-1,-1,-1)] for row in image]
+    print(f"Image dimensions: {len(image[0])}x{len(image)}")
+    for i in range(4):
+        dragons += count_dragons(image)
+        image = rotate(image)
+
+    roughness = 0
+    for row in image:
+        roughness += row.count("#")
+    return roughness - dragons*15
 
 with open("input.txt") as file:
     data = file.read()
+raw = data.split("\n\n")
+tiles = []
+for tile in raw:
+    t_id, info = tile.split(":")
+    t_id = int(t_id.split()[1])
+    t_image = list(info.split("\n")[1:])
+    tiles.append(Tile(t_id,t_image))
 
-res1 = part1(data)
-res2 = part2(data)
+res1 = part1(tiles)
+res2 = part2(tiles)
 print(f"Part 1: {res1}\nPart 2: {res2}")
