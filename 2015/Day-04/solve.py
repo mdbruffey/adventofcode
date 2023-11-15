638ff195270e470d2e8efa0024902ca1ec1db016
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-11-15 13:27:41 -0600

    Add solution for part 1 of Day-04

---
+++ 2015/Day-04/solve.py
@@ -0,0 +1,24 @@
+import hashlib
+
+def part1(key):
+    i = 1
+    while True:
+        result = hashlib.md5((key+str(i)).encode())
+        if result.hexdigest()[:5] == "00000":
+            return i
+        i += 1
+
+def part2(key):
+    i = 1
+    while True:
+        result = hashlib.md5((key+str(i)).encode())
+        if result.hexdigest()[:5] == "000000":
+            return i
+        i += 1
+
+with open("input.txt") as file:
+    key = file.read()
+
+res1 = part1(key)
+res2 = part2(key)
+print(f"Part 1: {res1}\nPart 2: {res2}")
