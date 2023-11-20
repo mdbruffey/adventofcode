255f681fd6bdfbb29bfe63ad1da39bc256deeaac
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-11-20 15:51:19 -0600

    Added most of the solution for Day-07 part 1
    Slight problem, my "solution" requires the listed connections
    to be "in order" in the sense that each wire needs to have been
    assigned already before the operation is done...
    
    Going to have to remake the solution structure entirely to account
    for that... (it was all in order in the short example, lol)

--- 2015/Day-07/solve.py
+++ 2015/Day-07/solve.py
@@ -1,12 +1,31 @@
-def execute(operation, target):
-    pass
+def execute(operation, target, wires):
+    print(operation)
+    if operation.isnumeric():
+        wires[target] = int(operation)
+    elif "AND" in operation:
+        left, right = operation.split(" AND ")
+        wires[target] = wires[left] & wires[right]
+    elif "OR" in operation:
+        left, right = operation.split(" OR ")
+        wires[target] = wires[left] | wires[right]
+    elif "RSHIFT" in operation:
+        left, right = operation.split(" RSHIFT ")
+        wires[target] = wires[left] >> int(right)
+    elif "LSHIFT" in operation:
+        left, right = operation.split(" LSHIFT ")
+        wires[target] = wires[left] << int(right)
+    elif "NOT" in operation:
+        wire = operation.split()[-1]
+        wires[target] = 65536 + ~wires[wire]
+    return wires
 
 def part1(data):
     wires = {}
     for line in data:
         left, target = line.split(" -> ")
-        execute(left, target, wires)
-    return wires["a"]
+        wires = execute(left, target.strip("\n"), wires)
+    print(wires)
+    return wires["x"]
 
 def part2(data):
     pass
