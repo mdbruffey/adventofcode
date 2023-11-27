369969ed9ec7da4b348595a1b98bb50783ce53dd
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-11-27 16:56:23 -0600

    Complete solution for Day-19 Part 2

--- 2015/Day-19/solve.py
+++ 2015/Day-19/solve.py
@@ -1,4 +1,13 @@
-def part1(molecule, options):
+import time
+
+def devolve(molecule, pairs):
+    for replace, target in pairs:
+        if replace in molecule:
+            idx = molecule.find(replace)
+            return molecule[:idx] + target + molecule[idx+len(replace):]
+    return molecule
+
+def part1(molecules, options):
     molecules = set()
     for i, char in enumerate(molecule):
         if char.islower():
@@ -12,7 +21,17 @@ def part1(molecule, options):
     return len(molecules)
 
 def part2(molecule, options):
-    pass
+    count = 0
+    inverse = {}
+    for key, value in options.items():
+        for replace in value:
+            inverse[replace] = key
+    pairs = list(inverse.items())
+    pairs.sort(key=lambda x: len(x[0]), reverse=True)
+    while molecule != "e":
+        molecule = devolve(molecule, pairs)
+        count += 1
+    return count
 
 with open("input.txt") as file:
     data = file.read()
@@ -26,7 +45,10 @@ for replacement in replacements:
         options[target] = [replace]
     else:
         options[target].append(replace)
-
+starta = time.perf_counter()
 res1 = part1(molecule, options)
+enda = time.perf_counter()
+startb = time.perf_counter()
+print(f"Part 1: {res1} -- {(enda-starta):.4f} s")
 res2 = part2(molecule, options)
-print(f"Part 1: {res1}\nPart 2: {res2}")
+print(f"Part 2: {res2} -- {(time.perf_counter()-startb):.4f} s")
