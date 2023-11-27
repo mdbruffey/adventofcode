6cefb0c7e89a2a6aa58d68d3f7efcba6fcf87c78
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-11-27 17:43:48 -0600

    Add solution for Day-20 Part 1

--- 2015/Day-20/solve.py
+++ 2015/Day-20/solve.py
@@ -1,40 +1,38 @@
 import math
+import itertools
+import time
 
 def get_factors(n):
     factors = []
-    # Print the number of two's that divide n 
     while n % 2 == 0: 
         factors.append(2), 
         n = n / 2
-          
-    # n must be odd at this point 
-    # so a skip of 2 ( i = i + 2) can be used 
     for i in range(3,int(math.sqrt(n))+1,2): 
-          
-        # while i divides n , print i and divide n 
         while n % i== 0: 
             factors.append(i), 
             n = n / i 
-              
-    # Condition if n is a prime 
-    # number greater than 2 
     if n > 2: 
-        factors.append(n)
+        factors.append(int(n))
     return factors 
 
 
 def calc_presents(n):
     factors = get_factors(n)
-    print(factors)
-    presents = 10
-    for num in set(factors):
-        for i in range(1,factors.count(num)+1):
-            presents += 10*num**i
-    return presents
+    count = 1 + sum(set(factors)) + n
+    for i in range(2,len(factors)):
+        combos = itertools.combinations(factors, i)
+        count += sum(map(lambda x: math.prod(x),set(combos)))
+    return count * 10
 
 
 def part1(data):
-    print(calc_presents(6))
+    n = 100
+    presents = 0
+    while presents < data:
+        n +=1
+        presents = calc_presents(n)
+    print(presents)
+    return n
 
 def part2(data):
     pass
@@ -42,6 +40,9 @@ def part2(data):
 with open("input.txt") as file:
     data = int(file.read())
 
+start1 = time.perf_counter()
 res1 = part1(data)
+print(f"Part 1: {res1} -- {time.perf_counter()-start1:.4f} s")
+start2 = time.perf_counter()
 res2 = part2(data)
-print(f"Part 1: {res1}\nPart 2: {res2}")
+print(f"Part 2: {res2} -- {time.perf_counter()-start2:.4f} s")
