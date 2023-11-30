c87f463ff160188a2fa7ed02d7d28a8ce1c60a16
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-11-29 20:39:57 -0600

    "Completed" the random approach to Part 1
    This doesn't work, lol. I need to implement a BFS approach
    instead, I think. That's something I need to practice, anyway.

--- 2016/Day-11/solve.py
+++ 2016/Day-11/solve.py
@@ -1,21 +1,65 @@
 import time
+import random
+
+def reset(data):
+    floors = {}
+    for key, value in data.items():
+        new = []
+        for item in value:
+            new.append(item)
+        floors[key] = new
+    return floors
 
 def is_fried(chip, floor, elevator, floors):
     flavor = chip.split("-")[0]
-    for item in floors[floor]:
-        if flavor in item:
+    for item in floors[floor] + elevator:
+        if flavor in item and item != chip:
             return False
-    for item in elevator:
-        if flavor in item:
-            return False
-    return True
+    for item in floors[floor] + elevator:
+        if "g" in item:
+            return True
+    return False
 
-def part1(floors):
-    total_items = sum([len(x) for x in floors.values()])
+def do_random_action(elevator, floor, floors):
+    for i in range(len(elevator)):
+        if random.randint(0,1):
+            floors[floor].append(elevator.pop(random.choice(range(len(elevator)))))
+    for i in range(2-len(elevator)):
+        if random.randint(0,1):
+            if len(floors[floor]):
+                elevator.append(floors[floor].pop(random.choice(range(len(floors[floor])))))
+    if not len(elevator):
+        elevator.append(floors[floor].pop(random.choice(range(len(floors[floor])))))
+    new_floor = random.choice([x+1 for x in range(4) if x+1 != floor])
+    for i in range(floor, new_floor, int((new_floor-floor)/abs(new_floor-floor))):
+        for item in floors[i] + elevator:
+            if "-m" in item:
+                if is_fried(item, i, elevator, floors):
+                    print(f"Something fried...")
+                    return -1
+    return new_floor
+
+def part1(data):
+    total_items = sum([len(x) for x in data.values()])
+    print(total_items)
     min_steps = 10000
-    for i in range(10000):
-        if len(floor["fourth"]) == total_items:
-            break
+    for i in range(1):
+        floors = reset(data)
+        print(f"Attempt {i}: ",end="")
+        steps = 0
+        elevator = []
+        curr_floor = 1
+        while True:
+            if steps > min_steps:
+                break
+            if len(floors[4]) == total_items:
+                break
+            curr_floor = do_random_action(elevator, curr_floor, floors)
+            if curr_floor < 1:
+                steps = 1000000
+            steps += 1
+        if steps < min_steps:
+            min_steps = steps
         
     return min_steps
 
@@ -25,11 +69,16 @@ def part2(data):
 with open("input.txt") as file:
     data = file.readlines()
 
+num_convert = {"first": 1,
+               "second": 2,
+               "third": 3,
+               "fourth": 4}
+
 floors = {}
 for line in data:
     floor, stuff = line.split(" floor contains ")
-    floor = floor.split()[-1]
-    if floor != "fourth":
+    floor = num_convert[floor.split()[-1]]
+    if floor != 4:
         if "," in stuff:
             stuff = stuff.split(", ")
         else:
@@ -43,8 +92,6 @@ for line in data:
     else:
         floors[floor] = []
 
-print(floors)
-        
 
 start = time.perf_counter()
 res1 = part1(floors)
