53c0d39948e2cd80de305304c29dc137426d46b8
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-12-01 14:44:35 -0600

    Complete extend function for part 1
    Doesn't support part 2 disk length. Need to fix.

--- 2016/Day-16/solve.py
+++ 2016/Day-16/solve.py
@@ -1,27 +1,31 @@
 import time
 import numpy as np
 
+def fix_padding(b, length, size):
+    for i in range(len(b)):
+        if len(b[i]) < 64:
+            if i < len(b)-1:
+                b[i] = "0"*(64-len(b[i])) + b[i]
+            else:
+                b[i] = "0"*(length-size-len(b[i])) + b[i]
+    return "".join(b)
+
 def extend(a):
     length = len(a)
     try:
         if length > 128:
-            b = []
             a_r = a[::-1]
-            print(a_r)
+            b = []
             b.append(bin(~np.uint64(int(a_r[:64], 2)))[2:])
             b.append(bin(~np.uint64(int(a_r[64:128], 2)))[2:])
             b.append(bin(~np.uint64(int(a_r[128:], 2)))[192-length +2:])
-            for i in range(len(b)):
-                if len(b[i]) < 64:
-                    if i < len(b)-1:
-                        b[i] = "0"*(64-len(b[i])) + b[i]
-                    else:
-                        b[i] = "0"*(length-128-len(b[i])) + b[i]
-            b = "".join(b)
-            print(b)
+            b = fix_padding(b, length, 128)
         elif length > 64:
             a_r = a[::-1]
-            b = bin(~np.uint64(int(a_r[:64], 2)))[2:] + bin(~np.uint64(int(a_r[64:], 2)))[128-length+2:]
+            b= []
+            b.append(bin(~np.uint64(int(a_r[:64], 2)))[2:])
+            b.append(bin(~np.uint64(int(a_r[64:], 2)))[128-length+2:])
+            b = fix_padding(b, length, 64)
         elif length > 32:
             b = bin(~np.uint64(int(a[::-1], 2)))[64-length+2:]
         else:
@@ -50,22 +54,16 @@ def part1(data, disk_length):
 
     return checksum
 
-def part2(data):
-    pass
-
 with open("input.txt") as file:
     data = file.read()
 
-disk_length = 20
-
-thing = "0"*63 + "1" + "0" + "1"*65
-
-extend(thing)
+disk_length_1 = 272
+disk_length_2 = 35651584
 
 start = time.perf_counter()
-#res1 = part1(data, disk_length)
-#print(f"Part 1: {res1} -- {time.perf_counter()-start:.4f} s")
+res1 = part1(data, disk_length_1)
+print(f"Part 1: {res1} -- {time.perf_counter()-start:.4f} s")
 start = time.perf_counter()
-res2 = part2(data)
+res2 = part1(data, disk_length_2)
 print(f"Part 2: {res2} -- {time.perf_counter()-start:.4f} s")
 
