0cd074761b1ffe145b301fa325ad5ce6bdb805aa
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-12-07 20:40:05 -0600

    Add solutions for Day-07 Part 1 & Part 2

--- 2023/Day-07/solve.py
+++ 2023/Day-07/solve.py
@@ -1,17 +1,106 @@
 import time
 
-def sort(hands):
-    pass
+def get_value(hand):
+    cards = {"A":.99,"K":.92,"Q":.85,"J":.78,"T":.71,"9":.64,"8":.56,
+         "7":.48,"6":.40,"5":.32,"4":.24,"3":.16,"2":.08,}
+    value = 0
+    unique = list(set(hand))
+    if len(unique) == 1:
+        value += 70
+    elif len(unique) == 2:
+        if hand.count(unique[0]) >= 2 and hand.count(unique[1]) >= 2:
+            #Full House
+            value += 50
+        else:
+            #Four of a kind
+            value += 60
+    elif len(unique) == 3:
+        for val in unique:
+            if hand.count(val) == 3:
+                #Three of a kind
+                value += 40
+                break
+        if not value:
+            #Two Pair
+            value += 30
+    elif len(unique) == 4:
+        value += 20
+    else:
+        value += 10
+    #trying to make a way to give values for each card in the hand... 
+    # a very weird equation that seems to work?
+    for i in range(len(hand)):
+        value += cards[hand[i]]/50**i
+    return value
+
+def get_value_v2(hand):
+    cards = {"A":.99,"K":.92,"Q":.85,"J":.01,"T":.71,"9":.64,"8":.56,
+         "7":.48,"6":.40,"5":.32,"4":.24,"3":.16,"2":.08,}
+    value = 0
+    unique = list(set(hand))
+    if len(unique) == 1:
+        value += 70
+    elif len(unique) == 2:
+        if "J" in unique:
+                value += 70 #Five of a kind
+        elif hand.count(unique[0]) >= 2 and hand.count(unique[1]) >= 2:
+            #Full House
+            value += 50
+        else:
+            #Four of a kind
+            value += 60
+    elif len(unique) == 3:
+        for val in unique:
+            if hand.count(val) == 3:
+                if "J" in unique: #Four of a kind
+                    value += 60
+                #Three of a kind
+                else:
+                    value += 40
+                break
+        if not value:
+            for val in unique:
+                if hand.count(val) == 2:
+                    if "J" in unique: 
+                        if hand.count("J") == 1:
+                            #Full House
+                            value += 50
+                        else: #Four of a kind
+                            value += 60
+                    else:
+                        #Two Pair
+                        value += 30
+                    break
+    elif len(unique) == 4:
+        if "J" in unique: #Three of a kind
+            value += 40
+        else:
+            value += 20 #One pair
+    else:
+        if "J" in unique: #One pair
+            value += 20
+        else:
+            value += 10
+    #trying to make a way to add additional value for each card in the hand
+    #for secondary ordering
+    for i in range(len(hand)):
+        value += cards[hand[i]]/50**i
+
+    return value
 
 def part1(hands):
-    hands = sort(hands)
+    hands.sort(key=lambda x: get_value(x[0]))
     count = 0
     for i in range(len(hands)):
         count += (i+1)*hands[i][1]
     return count
 
-def part2(data):
-    pass
+def part2(hands):
+    hands.sort(key=lambda x: get_value_v2(x[0]))
+    count = 0
+    for i in range(len(hands)):
+        count += (i+1)*hands[i][1]
+    return count
 
 with open("input.txt") as file:
     data = file.readlines()
