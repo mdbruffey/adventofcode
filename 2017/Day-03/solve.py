d6a6a2309718de6ce495b899c5c4a84f1f447427
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2024-09-23 13:39:55 -0500

    Add Day-03 empty solution file.

---
+++ 2017/Day-03/solve.py
@@ -0,0 +1,17 @@
+import time
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
+
+with open("input.txt") as file:
+    data = file.readlines()
+
+start = time.perf_counter()
+res1 = part1(data)
+print(f"Part 1: {res1} -- {time.perf_counter()-start:.4f} s")
+start = time.perf_counter()
+res2 = part2(data)
+print(f"Part 2: {res2} -- {time.perf_counter()-start:.4f} s")
