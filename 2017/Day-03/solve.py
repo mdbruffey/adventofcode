b12adc6d3a656d0a4e23168a6621638c2051cf16
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2024-09-23 14:09:52 -0500

    Complete Day 03 Part 1 and Part 2

--- 2017/Day-03/solve.py
+++ 2017/Day-03/solve.py
@@ -1,13 +1,70 @@
 import time
 
+direction = [
+    (1, 0),
+    (0, 1),
+    (-1, 0),
+    (0, -1)
+]
+
+neighbors = [
+    (1,0),
+    (1,1),
+    (0,1),
+    (-1,1),
+    (-1,0),
+    (-1,-1),
+    (0,-1),
+    (1,-1)
+]
+
 def part1(data):
-    pass
+    n = 1
+    x = 0
+    y = 0
+    dist = 1
+    dir = 0
+    while True:
+        for i in range(2):
+            for i in range(dist):
+                n += 1
+                x += direction[dir%4][0]
+                y += direction[dir%4][1]
+                if n == data:
+                    return x+y
+            dir += 1
+        dist += 1
+
+def get_cell_value(pos, grid):
+    value = 0
+    for neighbor in neighbors:
+        x = pos[0] + neighbor[0]
+        y = pos[1] + neighbor[1]
+        if (x,y) in grid:
+            value += grid[(x,y)]
+    return value
 
 def part2(data):
-    pass
+    grid = {}
+    x = 0
+    y = 0
+    dist = 1
+    dir = 0
+    grid[(x,y)] = 1
+    while True:
+        for i in range(2):
+            for i in range(dist):
+                x += direction[dir%4][0]
+                y += direction[dir%4][1]
+                value = get_cell_value((x,y), grid)
+                if value > data:
+                    return value
+                grid[(x,y)] = value
+            dir += 1
+        dist += 1
 
 with open("input.txt") as file:
-    data = file.readlines()
+    data = int(file.readlines()[0])
 
 start = time.perf_counter()
 res1 = part1(data)
