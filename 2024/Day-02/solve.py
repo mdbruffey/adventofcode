336533add1f5ce357202c8a9c6c40f4328898f3f
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2024-12-02 08:45:14 -0600

    Improve speed of Part 2 solution
    Modified is_safe() to return the starting index of the first unsafe
    interval in a report. Then dampened() simply checks the index 0 of
    the report, the reported starting index, and the next index to see if
    removing any one of those makes the report safe. This cuts the run-time
    for part 2 in half, roughly.

--- 2024/Day-02/solve.py
+++ 2024/Day-02/solve.py
@@ -1,35 +1,47 @@
 import time
 
+#returns a tuple (True, True) when the report is safe
+#When the report is not safe, the second entry of the tuple
+#contains the starting index of the unsafe interval
 def is_safe(report):
     ascending = report[0] < report[1]
     for i in range(len(report)-1):
         if ascending and report[i] >= report[i+1]:
-            return False
+            return False, i
         elif (not ascending) and report[i] <= report[i+1]:
-            return False
+            return False, i
         elif abs(report[i]-report[i+1]) > 3:
-            return False
-    return True
+            return False, i
+    return True, True
 
-def dampened(report):
-    for i in range(len(report)):
-        if is_safe(report[:i]+report[i+1:]):
+
+#Takes in a bad report and the starting index of the unsafe interval.
+#Operates on the basis that either the start or end of the interval is the
+#problem, so checks the report with those 2 removed. Also considers that
+#the very first level could be faulty, which could have incorrectly indicated
+#that the report was ascending or descending, so it always tries removing that
+#first.
+def dampened(report, idx):
+    for i in [0,idx,idx+1]:
+        if is_safe(report[:i]+report[i+1:])[0]:
             return True
     return False
+
 
 def part1(reports):
     count = 0
     for report in reports:
-        if is_safe(report):
+        if is_safe(report)[0]:
             count += 1
     return count
 
 def part2(reports):
     count = 0
     for report in reports:
-        if is_safe(report):
+        safe, i = is_safe(report)
+        if safe:
             count +=1
-        elif dampened(report):
+        elif dampened(report, i):
                 count += 1
     return count
 
