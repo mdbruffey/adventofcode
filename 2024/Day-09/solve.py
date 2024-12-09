3c8b76c82625a5e0da0b08f699f4ba25171f7e2d
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2024-12-09 10:41:49 -0600

    Add solutions for Day 09 Part 1 & 2

--- 2024/Day-09/solve.py
+++ 2024/Day-09/solve.py
@@ -1,13 +1,67 @@
 import time
 
 def part1(data):
-    pass
+    blocks = data[::2]
+    space = data[1::2]
+    i = 0
+    disk = []
+    while i < len(blocks) and i < len(space):
+        disk.extend([i]*int(blocks[i]))
+        disk.extend(["."]*int(space[i]))
+        i += 1
+    if i < len(blocks):
+        disk.extend([i]*int(blocks[i]))
+    j = len(disk)-1
+    count = 0
+    for i in range(len(disk)-disk.count(".")):
+        if disk[i] == ".":
+            while disk[j] == ".":
+                j -= 1
+            disk[i], disk[j] = disk[j], disk[i]
+        count += i*disk[i]
+    return count
 
 def part2(data):
-    pass
+    blocks = data[::2]
+    space = data[1::2]
+    i = 0
+    disk = []
+    sizes = {}
+    while i < len(blocks) and i < len(space):
+        disk.extend([i]*int(blocks[i]))
+        sizes[i] = int(blocks[i])
+        disk.extend(["."]*int(space[i]))
+        i += 1
+    if i < len(blocks):
+        disk.extend([i]*int(blocks[i]))
+        sizes[i] = int(blocks[i])
+    
+    for id in sorted(sizes,reverse=True):
+        index = disk.index(id)
+        size = sizes[id]
+        i = disk.index(".") #starting at the first index with space saves 5 seconds
+        while i < index:
+            if disk[i] == ".":
+                space = 1
+                while disk[i+space] == ".":
+                    space += 1
+                if space >= size:
+                    for j in range(size):
+                        disk[i+j], disk[index+j] = disk[index+j], disk[i+j]
+                    break
+                else:
+                    i += space
+            else:
+                i += 1
+    count = 0
+    for i in range(len(disk)):
+        if disk[i] != ".":
+            count += i*disk[i]
+    return count
+            
 
 with open("input.txt") as file:
-    data = file.readlines()
+    data = file.read()
 
 start = time.perf_counter()
 res1 = part1(data)
