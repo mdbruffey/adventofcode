fe0897ffd04befd89a9cef1b89965d60e5bfbc84
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2024-12-20 10:12:38 -0600

    Add solution for Day 20 Part 1 & 2

---
+++ 2024/Day-20/solve.py
@@ -0,0 +1,93 @@
+import time
+from queue import PriorityQueue
+
+directions = [(1,0),(0,1),(-1,0),(0,-1)]
+
+def manhattan(p1, p2):
+    return abs(p2[0]-p1[0]) + abs(p2[1]-p1[1])
+
+def get_neighbors(pos, walls, size, ignore):
+    x,y = pos
+    neighbors = []
+    for neighbor in [(x+dx, y+dy) for dx,dy in directions]:
+        if neighbor == ignore:
+            neighbors.append(neighbor)
+        elif neighbor in walls:
+            continue
+        elif 0 <= neighbor[0] < size[0] and 0 <= neighbor[1] < size[1]:
+            neighbors.append(neighbor)
+    return neighbors
+
+def find_path(start, end, walls, size, ignore=None):
+    queue = PriorityQueue()
+    comes_from = dict()
+    cost_till_now = dict()
+    comes_from[start] = None
+    cost_till_now[start] = 0
+    queue.put(start,0)
+    while not queue.empty():
+        curr = queue.get()
+        if curr == end:
+            break
+        for next in get_neighbors(curr, walls, size, ignore):
+            new_cost = cost_till_now[curr] + 1
+            if next not in cost_till_now or new_cost < cost_till_now[next]:
+                cost_till_now[next] = new_cost
+                comes_from[next] = curr
+                priority = new_cost + abs(end[0]-next[0]) + abs(end[1]-next[1])
+                queue.put(next, priority)
+    path = []
+    while curr:
+        path.append(curr)
+        curr = comes_from[curr]
+    path.reverse()
+    return path, cost_till_now
+
+def part1(data):
+    cheat_time = 2
+    cutoff = 100
+    walls = set()
+    size = (len(data[0]),len(data))
+    for i in range(size[1]):
+        for j in range(size[0]):
+            if data[i][j] == "#":
+                walls.add((j,i))
+            elif data[i][j] == "S":
+                start = (j,i)
+            elif data[i][j] == "E":
+                end = (j,i)
+    path, cost_till_now  = find_path(start, end, walls, size)
+    saved_times = {}
+    for i in range(len(path)-cutoff-cheat_time):
+        for j in range(i+cutoff+cheat_time,len(path)):
+            distance = manhattan(path[i],path[j])
+            if distance <= cheat_time:
+                time_saved = cost_till_now[path[j]] - cost_till_now[path[i]] - distance
+                saved_times[time_saved] = saved_times.get(time_saved, 0) + 1
+
+
+    return sum([saved_times[key] for key in saved_times if key >= cutoff]), path, cost_till_now
+
+
+def part2(path, cost_till_now):
+    cutoff = 100
+    cheat_time = 20
+    saved_times = {}
+    for i in range(len(path)-cutoff):
+        for j in range(i+cutoff,len(path)):
+            distance = manhattan(path[i],path[j])
+            if distance <= cheat_time:
+                time_saved = cost_till_now[path[j]] - cost_till_now[path[i]] - distance
+                saved_times[time_saved] = saved_times.get(time_saved, 0) + 1
+    print(max(saved_times.keys()))
+    return sum([saved_times[key] for key in saved_times if key >= cutoff])
+
+with open("input.txt") as file:
+    data = file.read().splitlines()
+
+start = time.perf_counter()
+res1, path, cost_till_now = part1(data)
+print(f"Part 1: {res1} -- {time.perf_counter()-start:.4f} s")
+start = time.perf_counter()
+res2 = part2(path, cost_till_now)
+print(f"Part 2: {res2} -- {time.perf_counter()-start:.4f} s")
